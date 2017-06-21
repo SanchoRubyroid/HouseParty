484fc7471a30ff783b2d6347bbc5bb8543ede68c
Alexander Areshnikov <alex.areshnikov@plansource.com>
2017-06-21 14:55:59 -0600

    Add Bulma and Font Awesome

--- app/assets/javascripts/components/application_box.jsx
+++ app/assets/javascripts/components/application_box.jsx
@@ -18,8 +18,46 @@ class ApplicationBox extends React.Component {
     render() {
         return (
             <div>
-                <div>Authorized: {this.state.authorized ? 'yep' : 'nope'}</div>
-                <div>Username: {this.state.username}</div>
+                <section className="hero is-info is-bold is-fullheight">
+                    <div className="hero-head">
+                        <div className="container">
+                            <div className="tabs is-centered">
+                                <ul>
+                                    <li className="is-active">
+                                        <a>
+                                        <span className="icon"><i className="fa fa-home"></i></span>
+                                        <span>Что происходит?</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a>
+                                            <span className="icon"><i className="fa fa-map-marker"></i></span>
+                                            <span>Как доехать?</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a>
+                                            <span className="icon"><i className="fa fa-gift"></i></span>
+                                            <span>Что взять?</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="hero-body">
+                        <div className="container has-text-centered">
+                            <h1 className="title">
+                                Title
+                            </h1>
+                            <h2 className="subtitle">
+                                Subtitle
+                            </h2>
+                        </div>
+                    </div>
+                </section>
+
             </div>
         );
     }
